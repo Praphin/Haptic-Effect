062311836571fdfd8d5eb4df701d27f63b1bb6d9
PSP <sppraphin@gmail.com>
2021-11-08 20:07:26 +0530

    Initial Commit

---
+++ Haptic Effect/Haptic_Effect.h
@@ -0,0 +1,18 @@
+//
+//  Haptic_Effect.h
+//  Haptic Effect
+//
+//  Created by Praphin SP on 08/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Haptic_Effect.
+FOUNDATION_EXPORT double Haptic_EffectVersionNumber;
+
+//! Project version string for Haptic_Effect.
+FOUNDATION_EXPORT const unsigned char Haptic_EffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Haptic_Effect/PublicHeader.h>
+
+
